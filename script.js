83ad7d1d781a1bc14e6c7cedddb3c2ead7d51152
bacemlm <128023177+bacemlm@users.noreply.github.com>
2026-01-14 14:18:18 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const hamburger = document.getElementById("hamburger");
   });
 
 window.addEventListener("load", () => {
-    const loader = document.getElementById("loader");
+    loader = document.getElementById("loader");
     loader.style.opacity = "0";
     loader.style.transition = "2s ease";
     setTimeout(() => loader.remove(), 500);
@@ -24,7 +24,7 @@ window.addEventListener("load", () => {
 
 function type() {
     typed = new Typed('.multiple-text', {
-        strings: ["Avicenne Consulting"],
+        strings: ["AVICENNE Consulting"],
         typeSpeed: 100,
         backSpeed: 100,
         backDelay: 1000,
@@ -61,6 +61,50 @@ function controles() {
         return true;
     }
 }
+function openOffre() {
+    document.getElementById("popup").classList.add("open-popup");
+    document.getElementById("page-content").classList.add("blur");
+}
+function salaireRange() {
+    salaire = document.getElementById("salaire");
+    salaireValue = document.getElementById("salaireValue");
+    salaire.addEventListener("input", () => {
+        salaireValue.textContent = salaire.value;
+    });
+}
+function offre() {
+    nom = document.getElementById("name").value;
+    prenom = document.getElementById("prenom").value;
+    email = document.getElementById("email").value;
+    cv = document.getElementById("cv").value;
+    popup1=document.getElementById("popup1");
+    popup2=document.getElementById("popup2");
+
+    if(nom.length=="") {
+        document.getElementById("popup-txt").textContent = "Veuillez entrer votre nom.";
+        popup1.classList.add("open-popup");
+        return false;
+    }else if(prenom.length=="") {
+        document.getElementById("popup-txt").textContent = "Veuillez entrer prénom.";
+        popup1.classList.add("open-popup");
+        return false;
+    }else if (email.indexOf("@")== -1 || email.indexOf(".")== -1) {
+        document.getElementById("popup-txt").textContent = "Veuillez entrer une addresse email valide.";
+        popup1.classList.add("open-popup");
+        return false;
+    }else if (cv == "") {
+        document.getElementById("popup-txt").textContent = "Veuillez ajouter un cv.";
+        popup1.classList.add("open-popup");
+        return false;
+    }else{
+        document.getElementById("popup").classList.remove("open-popup");
+        document.getElementById("page-content").classList.remove("blur");
+        popup2.classList.add("open-popup");
+        document.getElementById("page-content").classList.add("blur");
+        return true;
+    }
+}
+
 function closePopup(id) {
     document.getElementById(id).classList.remove("open-popup");
     document.getElementById("page-content").classList.remove("blur");
